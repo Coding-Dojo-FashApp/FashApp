39789907b66459149ffa5d5502f683415d97b85f
Kevin McGrath <kpmcgra@gmail.com>
2023-02-01 20:14:35 -0600

    route for create_outfit has a function that returns an array of clothing item ids

--- FashApp/controllers/outfits.py
+++ FashApp/controllers/outfits.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from FashApp import app
 from flask import render_template, request, redirect, session, flash
 from FashApp.models import user
@@ -8,15 +7,10 @@ from FashApp.models import clothing_item
 from datetime import datetime
 
 
-
-=======
->>>>>>> 394802e6b98e3b205a6a554b736021075ab4f940
-
 @app.route("/new_outfit")
 def new_outfit():
     current_user = user.User.get_one(session['users_id'])
     clothing_by_category = {}
-<<<<<<< HEAD
     all_category = clothing_category.Clothing_catagories.get_all()
     for category in all_category:
         clothing_by_category[category.name] = clothing_item.Clothing_items.get_clothing_by_category(category.id)
@@ -38,21 +32,13 @@ def remove_first(array):
 @app.route("/create_outfit", methods = ['POST'])
 def save_new_outfit():
     clothingIdArray = []
-    print(request.form)
     dictionary = request.form.to_dict()
     print(dictionary)
     for category in dictionary:
             clothingIdArray.append(dictionary[category])
-    return(redirect('/'))
+    clothingIdArray = remove_first(clothingIdArray)
+    print(clothingIdArray)
+    # clothing id array contains the ids of the clothing items selected
+    return(redirect('/home'))
 
 
-
-=======
-    categories = clothing_category.Clothing_catagories.get_all()
-    for category in categories:
-        clothing_by_category[category.name] = clothing_item.Clothing_items.get_clothing_by_category(category.id)
-    print("\n__clothing by category", clothing_by_category)
-    for clothing in clothing_by_category:
-        print("Clothing in category", clothing_by_category[clothing])
-    return render_template("create_outfit.html", current_user = current_user, clothing_by_category = clothing_by_category)
->>>>>>> 394802e6b98e3b205a6a554b736021075ab4f940
